ebae430fbb2994d130249fda2bb305bdae3e8ee8
Zhao.Kai <zeakey@126.com>
2015-12-01 20:26:42 +0800

    score4->score1

--- examples/fcnn/test_fcn11.m
+++ examples/fcnn/test_fcn11.m
@@ -1,4 +1,3 @@
-
 caffe_root = '../../';
 addpath(genpath([caffe_root,'matlab/']));
 use_gpu = 1;
@@ -15,7 +14,7 @@ test_imgs_dir = '../../data/figure/';
 model_dir = './';
 %model_dir = '../../examples/finetune/';
 net_model = [model_dir 'fcn11_deploy.prototxt'];
-net_weights = [model_dir 'models/fcn11/fcn11s_iter_75000.caffemodel'];
+net_weights = [model_dir 'models/fcn11/model_iter_15000.caffemodel'];
 phase = 'test'; % run with phase test (so that dropout isn't applied)
 if ~exist(net_weights, 'file')
   error('Please download CaffeNet from Model Zoo before you run this demo');
@@ -26,7 +25,6 @@ net = caffe.Net(net_model, net_weights, phase);
 test_imgs = dir([test_imgs_dir,'*.jpg']);
 test_imgs = {test_imgs.name};
 for i = 1:length(test_imgs),test_imgs{i} = test_imgs{i}(1:end-4);end
-
 for i = 1:length(test_imgs)
     im = imread([test_imgs_dir,test_imgs{i},'.jpg']);
     siz=size(im);
@@ -34,19 +32,18 @@ for i = 1:length(test_imgs)
     oldshape=blobdata.shape;
     newshape=[siz(2),siz(1),oldshape(3),oldshape(4)];
     blobdata.reshape(newshape);
-
     im_data = im(:, :, [3, 2, 1]);   % permute channels from RGB to BGR
     im_data = permute(im_data, [2, 1, 3]);  % flip width and height
     im_data = single(im_data);       % convert from uint8 to single
-
     scores = net.forward({im_data});
     s = scores{1};
     figure(1);
     subplot(3, 4, 1); imshow(im);
     for j = 1:10
-       subplot(3, 4, j+1);imshow(s(:,:,j)') 
+       subplot(3, 4, j + 1);
+       imshow(s(:,:,j)');
+       title(num2str(j - 1));
     end
+    colormap('hot');
 end
-
-
 caffe.reset_all();
